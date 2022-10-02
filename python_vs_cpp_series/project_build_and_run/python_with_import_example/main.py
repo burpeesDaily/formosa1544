eeea76a9317e514f36caaf56f0a4eb2b557a718a
Shun Huang <zsh@shunsvineyard.info>
2022-10-02 12:33:34 -0700

    Add more comments.

--- python_vs_cpp_series/project_build_and_run/python_with_import_example/main.py
+++ python_vs_cpp_series/project_build_and_run/python_with_import_example/main.py
@@ -4,6 +4,6 @@ import mylib
 if __name__ == "__main__":
     number_1 = 10
     number_2 = 20
-
     result = mylib.add(a=number_1, b=number_2)
+    print("Running from main...")
     print(f"{number_1} + {number_2} = {result}")
